efbc6effa64b4d570a3b3b92a99cfd80033217fe
Dhanush <dhanush.11ror@gmail.com>
2018-09-25 08:59:21 +0530

    updated mongoose version

--- config/db.js
+++ config/db.js
@@ -8,7 +8,7 @@ mongoose.Promise = global.Promise;
 // })
 
 //mongoose.connect(mongodb://dhanush17:mlab17@ds131258.mlab.com:31258/dct)
-mongoose.connect('mongodb://dhanush17:mlab17@ds131258.mlab.com:31258/dct', { useMongoClient: true }).then(() => {
+mongoose.connect('mongodb://dhanush17:mlab17@ds131258.mlab.com:31258/dct', {  useNewUrlParser: true }).then(() => {
     console.log("DB is connected")
 }).catch((err) => {
   console.log(err)
